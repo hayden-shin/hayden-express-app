9dbd4197641c7ca2d41c25abc04972a80c4b33f4
hayden <haydenhyo@gmail.com>
2025-01-15 06:43:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+	res.send('CICD TESTING 🦋');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is listening at port ${PORT}`);
+});
